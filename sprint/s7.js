cf7dcc622186e47cded9eb8cdadd191fd9f64aad
Laurion Dareich <klauriondareich@gmail.com>
2019-02-18 15:50:15 +0100

    correction du programme

--- sprint/s7.js
+++ sprint/s7.js
@@ -1,4 +1,7 @@
-let nombre = prompt("saisir un nombre");
-while (nombre > 0) {
+//programme qui demande de saisir un nombre tant que le nombre saisi n’est pas négatif.
+let nombre;
+do  {
 	nombre = prompt("saisir un nombre");
 }
+while (nombre > 0);
+	

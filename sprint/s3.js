efcccc2e6963267bf53a6ad8f4fc95f3b32793e6
Laurion Dareich <klauriondareich@gmail.com>
2019-02-18 13:28:28 +0100

    indentation du code

--- sprint/s3.js
+++ sprint/s3.js
@@ -1,4 +1,8 @@
 //Ce programme affiche Supérieur si  un nombre est supérieur à 10 sinon affiche Inférieur le cas échéant.
 let nombre = 11;
-if (nombre > 10) {console.log("Supérieur");}
-else {console.log("Inférieur");}
+if (nombre > 10) {
+	console.log("Supérieur");
+}
+else {
+	console.log("Inférieur");
+}

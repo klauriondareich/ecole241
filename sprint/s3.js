b28f25a5b1d90f71bae580f15eb03fc9a52f206a
Laurion Dareich <klauriondareich@gmail.com>
2019-02-18 13:18:28 +0100

    identation du code  s3.js

--- sprint/s3.js
+++ sprint/s3.js
@@ -1,8 +1,4 @@
-//Ce programme affiche 22 fois le message OKACODE
+//Ce programme affiche Supérieur si  un nombre est supérieur à 10 sinon affiche Inférieur le cas échéant.
 let nombre = 11;
-if (nombre > 10) {
-	console.log("Supérieur");
-}
-else {
-  console.log("Inférieur");
-}
+if (nombre > 10) {console.log("Supérieur");}
+else {console.log("Inférieur");}

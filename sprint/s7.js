07d29628d7ea64e809975f8ad17a4ca5ebc79328
Laurion Dareich <klauriondareich@gmail.com>
2019-02-18 15:03:47 +0100

    ajout du fichier s7.js

--- sprint/s7.js
+++ sprint/s7.js
@@ -1,5 +1,3 @@
-//programme qui demande de saisir un nombre tant que le nombre saisi n’est pas négatif
-
 let nombre = prompt("saisir un nombre");
 while (nombre > 0) {
 	nombre = prompt("saisir un nombre");

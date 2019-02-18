cd90e93b955541707e4dbb6f76cc1fa334399687
Laurion Dareich <klauriondareich@gmail.com>
2019-02-18 14:08:34 +0100

    ajout du fichier s5.js

---
+++ sprint/s5.js
@@ -0,0 +1,10 @@
+////Ce programme demande deux nombres et affiche le plus grand.
+let a,b;
+a = prompt("Entrer le premier nombre");
+b = prompt("Entrer le deuxième nombre");
+if (a>b) {
+  console.log(a + " est plus grand");
+}
+else {
+  console.log( b + " est plus grand");
+}

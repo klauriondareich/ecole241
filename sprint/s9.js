26b82faed4e697371bed4189a36daa5187549818
Laurion Dareich <klauriondareich@gmail.com>
2019-02-25 16:36:58 +0100

    Indentation du code

--- sprint/s9.js
+++ sprint/s9.js
@@ -1,9 +1,9 @@
 let nb,max;
-max = Number(prompt("saisir le nombre numero 1" ));
-for (i = 2;i<=5;i++) {
-	nb = Number(prompt("Saisir le nombre" + i));
-	if (nb > max) {
-		max = nb;
-	} 
+max = Number(prompt("Saisir le nombre numero 1" ));
+for (i = 2; i<=5; i++){
+  nb = Number(prompt("Saisir le nombre" + i));
+  if (nb > max){
+  	max = nb;
+  } 
 };
-console.log(`le plus grand est ${max}`)	;
+console.log(`le plus grand est ${max}`);

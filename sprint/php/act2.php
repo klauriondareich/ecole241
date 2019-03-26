93f213f19f70bfa0ff3130b0ead6277d1a024aae
Laurion Dareich <klauriondareich@gmail.com>
2019-03-26 15:03:04 +0100

    ajout du fichier act2 dans le dossier php

---
+++ sprint/php/act2.php
@@ -0,0 +1,14 @@
+<?php
+// les types de variables PHP
+ $number = 12; // int
+ $name = 'laurion'; // string
+ $table = array('12','14','15'); // array
+ $decimal = 12.14; // float
+ $none = Null; // Null
+ $answer = true; // true
+ echo gettype $number .'<br />'; 
+ echo gettype $name .'<br />';
+ echo gettype $table .'<br />';
+ echo gettype $decimal .'<br />';
+ echo gettype $answer .'<br />';
+?>

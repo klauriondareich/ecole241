0fd8d4f843f4b7d515a95bfa24e055109ebdc82b
Laurion Dareich <klauriondareich@gmail.com>
2019-03-27 10:18:45 +0100

    ajout du fichier act3 dans le dossier php

---
+++ sprint/php/act3.php
@@ -0,0 +1,8 @@
+<?php
+// Les tableaux
+$table = array('laurion','dareich','agnesvie','jespère','immaculée','rodalsi','béni-soi','prinsly','dane','rhudel');
+echo 'La liste des 1O prénoms <br />';
+foreach($table as $value) {
+	echo $value .'<br />';
+}
+?>
